e82f44c03d1051a4dc0735f83e1cb99e29d5265e
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-12-14 00:45:11 +0600

    updat port problem

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,17 @@
-/* eslint-disable no-console */
-import dotenv from "dotenv";
-import path from "path";
 import { Server } from "http";
 import app from "./app";
-import envVars from "./config/env";
 import { seedAdmin } from "./helpers/seed";
-dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 let server: Server;
 
 async function main() {
   try {
-    const port = Number(process.env.PORT) || envVars.PORT;
+    const port = Number(process.env.PORT) || 5000;
 
-server = app.listen(port, () => {
-  console.log(`🚀 Tourify Server is running on port ${port}`);
-});
+    server = app.listen(port, () => {
+      console.log(`🚀 Tourify Server is running on port ${port}`);
+    });
 
-
-    // Optional: Seed Super Admin
-    // await seedSuperAdmin();
     await seedAdmin();
   } catch (err) {
     console.log("Error starting server:", err);
@@ -27,47 +19,3 @@ server = app.listen(port, () => {
 }
 
 main();
-
-// Handle Unhandled Rejection (Async code errors)
-process.on("unhandledRejection", (err) => {
-  console.log(`😈 Unhandled Rejection is detected, shutting down ...`, err);
-  if (server) {
-    server.close(() => {
-      process.exit(1);
-    });
-  } else {
-    process.exit(1);
-  }
-});
-
-// Handle Uncaught Exception (Sync code errors)
-process.on("uncaughtException", (err) => {
-  console.log(`😈 Uncaught Exception is detected, shutting down ...`, err);
-  process.exit(1);
-});
-
-// SIGINT: Triggered by Ctrl+C
-process.on("SIGINT", () => {
-  console.log("SIGINT signal received... Server shutting down..");
-  if (server) {
-    server.close(() => {
-      console.log("Server closed gracefully.");
-      process.exit(0); // Success exit
-    });
-  } else {
-    process.exit(0);
-  }
-});
-
-// SIGTERM: Triggered by termination signal (e.g., from OS or Docker)
-process.on("SIGTERM", () => {
-  console.log("SIGTERM signal received... Server shutting down..");
-  if (server) {
-    server.close(() => {
-      console.log("Server closed gracefully.");
-      process.exit(0); // Success exit
-    });
-  } else {
-    process.exit(0);
-  }
-});
